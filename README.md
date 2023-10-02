d1168f09f22c6a044b2de807b5af92589b458837
Sampsa Penna <sampsapenna@gmail.com>
2023-10-02 19:45:10 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pico SDK example using the submodule approach
+
+## Getting started
+Repository contains `pico-sdk` as a submodule. After cloning the
+repository, run the following:
+
+```
+git submodule init && git submodule foreach "git submodule update --init"
+```
+
+## Usage
+```
+mkdir build
+cd build
+cmake ..
+make
+```
---
+++ serial_hello.c
@@ -0,0 +1,30 @@
+// Generic example program. Blinks the pico LED, and reports
+// the program runtime to serial.
+
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+
+int setup()
+{
+	// Set up stdio
+	stdio_init_all();
+	printf("Initialized standard IO\n");
+	gpio_init(PICO_DEFAULT_LED_PIN);
+	gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+	return 0;
+}
+
+int main() {
+	setup();
+	unsigned int uptime = 0;
+	while (true) {
+		printf("Uptime: %d seconds.\n", uptime);
+		gpio_put(PICO_DEFAULT_LED_PIN, 1);
+		sleep_ms(500);
+		gpio_put(PICO_DEFAULT_LED_PIN, 0);
+		sleep_ms(500);
+		uptime++;
+	}
+}
